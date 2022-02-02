31fdc47edbef522744c642d9146ba5f665025ba5
xdraffa <rafaelazevedopacheco4@gmail.com>
2022-02-01 21:27:33 -0300

    First Commit

--- jogo.js
+++ jogo.js
@@ -1,6 +1,3 @@
-console.log('[DevSoutinho] Flappy Bird');
-console.log('Inscreva-se no canal :D https://www.youtube.com/channel/UCzR2u5RWXWjUh7CwLSvbitA');
-
 const sprites = new Image();
 sprites.src = '/sprites.png';
 
